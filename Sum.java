b009e82d1d156e58459a97c224c6c6d5357d0320
Sabiq Sayed <87897829+sabiq12@users.noreply.github.com>
2026-02-16 09:30:56 +0530

    Sum.java
    Fix syntax errors and implement sum calculation.

---
+++ Sum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Sum
+  {
+    public static void main(string args[])
+    {
+      Scanner s = new Scanner
+      int a=s.nextInt();
+      int b=s.nextInt();
+      int sum =a+b;
+      System.out.println("the sum of 2 no.="+sum)
+    }
+  }
